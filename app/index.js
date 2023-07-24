9889c43e93e1df9cc13e9b168d89a19e2b6b4fa5
KunaalAgarwal <aad5ha@virginia.edu>
2023-07-23 22:30:38 -0400

    added error handling and event handlers to scoring format page

--- app/index.js
+++ app/index.js
@@ -1,47 +1,51 @@
 const ids = ["sf", "ros", "tc", "results"];
+let scoringFormat;
 let currPageId = "sf";
+let rosterConstruction = {"QB": 0, "RB": 0, "WR": 0, "TE": 0, "FLEX": 0, "BENCH": 0};
 setup();
 
 function displayPage(id) {
     currPageId = id;
     navbarUnderline(id);
     ids.forEach(page => {
-        if (id === page) {
-            document.getElementById(page + "content").style.display = "block";
-        } else {
-            document.getElementById(page + "content").style.display = "none";
-        }
-    })
-    if (currPageId === "sf") {
-        document.getElementById("prev").style.display = "none";
-    } else {
-        document.getElementById("prev").style.display = "block";
-    }
-    if (currPageId === "results") {
-        document.getElementById("next").style.display = "none";
-    } else {
-        document.getElementById("next").style.display = "block";
-    }
+        document.getElementById(`${page}content`).style.display = id === page ? "block" : "none";
+    });
+    document.getElementById("prev").style.display = currPageId === "sf" ? "none" : "block";
+    document.getElementById("next").style.display = currPageId === "results" ? "none" : "block";
 }
-
 
 function navbarUnderline(id) {
     ids.forEach(x => {
-        let element = document.getElementById(x);
-        if (x === id) {
-            element.style.borderBottom = "3px solid black";
-        } else {
-            element.style.borderBottom = "";
-        }
+        const element = document.getElementById(x);
+        element.style.borderBottom = x === id ? "3px solid black" : "";
     });
 }
 
 function next() {
     const currIndex = ids.indexOf(currPageId);
+    if (currPageId === "sf") {
+        if (sfNextCheck()) {return;}
+    }
     if (currIndex + 1 < ids.length) {
         const newPage = ids[currIndex + 1];
         displayPage(newPage);
     }
+}
+
+function sfNextCheck(){
+    let hasErrors = false;
+    if (scoringFormat === undefined){
+        document.getElementById("scoringButton-error").textContent = "Please select a scoring format."
+    } else {
+        document.getElementById("scoringButton-error").textContent = "";
+    }
+    setRosterConstruction();
+    document.querySelectorAll(".error-message").forEach(error => {
+        if (!(error.textContent === "")){
+            hasErrors = true;
+        }
+    })
+    return hasErrors;
 }
 
 function prev() {
@@ -54,4 +58,30 @@ function prev() {
 
 function setup() {
     displayPage("sf");
+}
+
+function parseScoringFormat(id) {
+    const scoringButtons = document.querySelectorAll(".scoringButton");
+    scoringButtons.forEach(button => {
+        button.style.borderRadius = button.id === id ? "18px" : "10px";
+        button.style.color = button.id === id ? "#fff" : "#666666";
+    });
+    scoringFormat = id.toUpperCase();
+}
+
+function parseRosterCon(id) {
+    const element = document.getElementById(id);
+    const positionSlots = parseInt(element.value, 10);
+    if (positionSlots < element.min || positionSlots > element.max || isNaN(positionSlots)) {
+        document.getElementById(`${id}-error`).textContent = "Please enter a valid number.";
+    } else {
+        document.getElementById(`${id}-error`).textContent = "";
+    }
+    return positionSlots;
+}
+
+function setRosterConstruction() {
+    for (let position in rosterConstruction) {
+        rosterConstruction[position] = parseRosterCon(`${position.toLowerCase()}-slots`);
+    }
 }
